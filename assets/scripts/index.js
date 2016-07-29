f7c112db8d7918a8b2fa6eaa6307b6da6f644be8
modularjon <modularjon@gmail.com>
2016-07-29 15:07:49 -0400

    On document ready, install paper and add handler

--- assets/scripts/index.js
+++ assets/scripts/index.js
@@ -1,7 +1,8 @@
 'use strict';
 
-// user require with a reference to bundle the file and use it in this file
-// var example = require('./example');
+const paperSetup = require('./paper/paper-setup');
 
-// use require without a reference to ensure a file is bundled
-require('./example');
+$(() => {
+  paper.install(window);
+  paperSetup.addHandlers();
+});
