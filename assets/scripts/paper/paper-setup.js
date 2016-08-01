02b4dddd383d693d481ac983d4f6edf62033f773
modularjon <modularjon@gmail.com>
2016-08-01 10:54:43 -0400

    Add exportSVG, getPaletteColor functions
    getPaletteColor retrieves css color value of palette and stores it in global
    pixelColor variable
    
    exportSVG currently saves exported SVG as a string

--- assets/scripts/paper/paper-setup.js
+++ assets/scripts/paper/paper-setup.js
@@ -2,29 +2,32 @@
 
 const paper = require('paper');
 
+// literal definition in lieu of a future pallette
+let pixelColor = 'red';
+
+// variable to hold exported svg
+let svg;
+
 const paperSetup = () => {
   // Get reference to canvas object:
   // let canvas = document.getElementById('myCanvas');
   // Create empty project and view for the canvas:
   paper.setup('myCanvas');
 
-// set canvas to 256 x 256 pixels
+  // set canvas to 256 x 256 pixels
   let canvasDimension = 256;
 
   paper.view.viewSize = (canvasDimension, canvasDimension);
 
-// literal definition in lieu of a future pallette
-  let pixelColor = 'red';
-
-// define the grid and include a bad click handler for now
+  // define the grid and include a bad click handler for now
   const drawGridRects = function(gridWidth, gridHeight, canvasSize) {
     let widthPixels = canvasSize.width / gridWidth;
     let heightPixels = canvasSize.height / gridHeight;
     for (let i = 0; i < gridWidth; i++) {
       for (let j = 0; j < gridHeight; j++) {
         let paperPixel = new paper.Path.Rectangle(canvasSize.left + i * widthPixels, canvasSize.top + j * heightPixels, widthPixels, heightPixels);
-        paperPixel.strokeColor = 'white';
-        paperPixel.fillColor = 'black';
+        paperPixel.strokeColor = 'black';
+        paperPixel.fillColor = 'white';
 
         paperPixel.onClick = function(event) {
           this.fillColor = pixelColor;
@@ -33,15 +36,33 @@ const paperSetup = () => {
     }
   };
 
-// creates the grid
+  // creates the grid
   drawGridRects(16, 16, paper.view.bounds);
 
-// draws the view
+  // draws the view
   paper.view.draw();
+};
+
+const exportSVG = function() {
+  svg = paper.project.exportSVG({asString: true});
+  console.log(svg);
+};
+
+const importSVG = function() {
+
+};
+
+const getPalletteColor = function(event) {
+  console.log(event.target);
+  console.log($(event.target).css('background-color'));
+  pixelColor = $(event.target).css('background-color');
 };
 
 const addHandlers = () => {
   paperSetup();
+  $('.exportSVG').on('click', exportSVG);
+  $('.importSVG').on('click', importSVG);
+  $('.pallette').on('click', getPalletteColor);
 };
 
 module.exports = {
