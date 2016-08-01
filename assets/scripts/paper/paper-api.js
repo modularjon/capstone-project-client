40e87fe079710b9fd7a9db7dd2fafd3ec39edfdb
modularjon <modularjon@gmail.com>
2016-08-01 16:38:03 -0400

    Add initial ajax funcs for indexPosts/createPost

---
+++ assets/scripts/paper/paper-api.js
@@ -0,0 +1,29 @@
+'use strict';
+
+const app = require('../app.js');
+
+const indexPosts = () => {
+  return $.ajax({
+    url: app.host + '/posts',
+    method: 'GET',
+    headers: {
+      Authorization: 'Token token=' + app.user.token,
+    },
+  });
+};
+
+const createPost = (data) => {
+  return $.ajax({
+    url: app.host + '/posts',
+    method: 'POST',
+    data,
+    headers: {
+      Authorization: 'Token token=' + app.user.token,
+    },
+  });
+};
+
+module.exports = {
+  indexPosts,
+  createPost,
+};
