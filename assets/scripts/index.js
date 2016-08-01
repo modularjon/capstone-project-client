053d6961226e194164348672fc8444a06a696a38
modularjon <modularjon@gmail.com>
2016-08-01 15:15:12 -0400

    Add authEvents handlers on page load

--- assets/scripts/index.js
+++ assets/scripts/index.js
@@ -1,8 +1,10 @@
 'use strict';
 
 const paperSetup = require('./paper/paper-setup');
+const paper = require('paper');
 
 $(() => {
   paper.install(window);
   paperSetup.addHandlers();
+  authEvents.addHandlers();
 });
