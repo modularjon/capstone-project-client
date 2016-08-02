ab222a1a902a326adc833a78d420c2f34f1ce8fe
modularjon <modularjon@gmail.com>
2016-08-02 11:15:22 -0400

    Add boilerplate for paper-ui

---
+++ assets/scripts/paper/paper-ui.js
@@ -0,0 +1,20 @@
+'use strict';
+
+const app = require('../app.js');
+
+const success = (data) => {
+  if (data) {
+    console.log(data);
+  } else {
+    console.log('Success');
+  }
+};
+
+const failure = (error) => {
+  console.error(error);
+};
+
+module.exports = {
+  success,
+  failure,
+};
